db8cf7cf7534ea6c2046f7c33add500e69515765
igor.bagliotti <igorbag@hotmail.com>
2022-10-05 09:12:19 -0300

    Commit desafio alunos do curso Entendendo a Estrutura e Depurando Apps Android

---
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.igorbag.meuprimeiroappdio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //O desafio sera criar uma valor dentro do string.xml
+        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+    }
+}
